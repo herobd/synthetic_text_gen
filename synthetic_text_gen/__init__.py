83cb2e541d45edb0afd11243b9f33b0691cba814
Brian Davis <herobd@gmail.com>
2021-09-03 15:56:35 -0600

    fix

--- synthetic_text_gen/__init__.py
+++ synthetic_text_gen/__init__.py
@@ -1,5 +1,5 @@
 from .synthetic_text import SyntheticText
-from .synthetic_text import SyntheticWord
+from .synthetic_word import SyntheticWord
 from .synthetic_text import apply_tensmeyer_brightness
 from . import grid_distortion
 from . import img_f
